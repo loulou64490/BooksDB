23281d2eaa858d42e40a273a557fbbc645822d13
Loulou <louis@revuejazz.fr>
2024-10-04 12:05:00 +0200

    New version

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # BookDB
 ## A small Flask app to manage a book database
-That's a school project to learn Flask and SQL. The app is a simple book database where you can add, edit, delete and search for books. The database is a SQLite database.
+School project to learn Flask and SQLite
--- app.py
+++ app.py
@@ -1,70 +1,83 @@
 import sqlite3
-import ollama #TODO : ajouter de l'ia
+import ollama  # TODO : ajouter de l'ia
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
 
 def get_db_connection():
     conn = sqlite3.connect('books.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+# TODO: page d'accueil
 @app.route('/')
 def accueil():
     return render_template('index.html', )
 
 
-@app.route('/nouveau')
-def nouveau():
-    return render_template('nouveau.html', )
-
-@app.route('/ajouter', methods=['POST'])
-def ajouter():
-    conn=get_db_connection()
-    cur=conn.cursor()
-    cur.execute("select * from books where title like ? or author like ? or year like ? or comment like ?",)
-    data=cur.fetchall()
-    conn.commit()
-    cur.close()
-    conn.close()
-    return redirect("livre?",303)
-
-@app.route('/livre',methods=['GET'])
+@app.route('/livre', )
 def livre():
-    conn=get_db_connection()
-    cur=conn.cursor()
-    data=cur.execute("select * from books where id=?",[request.args.get('id')]).fetchone()
-    cur.close()
-    conn.close()
-    return render_template('livre.html', data=data)
+    if request.args.get('id'):
+        conn = get_db_connection()
+        cur = conn.cursor()
+        data = cur.execute("select * from books where id=?", [request.args.get('id')]).fetchone()
+        cur.close()
+        conn.close()
+        return render_template('livre.html', data=data)
+    else:
+        return redirect("recherche", 303)
 
-@app.route('/recherche', methods=['POST'])
+
+@app.route('/recherche', methods=['POST', 'GET'])
 def recherche():
-    conn=get_db_connection()
-    cur=conn.cursor()
-    data=cur.execute("select * from books where title like ? or author like ? or year like ? or comment like ?",['%'+request.form['query']+'%' for i in range(4)]).fetchall()
-    cur.close()
-    conn.close()
-    return render_template('recherche.html', data=data, result=request.form['query'])
+    if request.form['query'] != '':
+        conn = get_db_connection()
+        cur = conn.cursor()
+        data = cur.execute("select * from books where title like ? or author like ? or year like ? or comment like ?",
+                           ['%' + request.form['query'] + '%' for i in range(4)]).fetchall()
+        cur.close()
+        conn.close()
+        return render_template('recherche.html', data=data, result=request.form['query'])
+    else:
+        return render_template('recherche.html', )
 
-@app.route('/supprimer', methods=['POST'])
-def supprimer():
-    conn=get_db_connection()
-    cur=conn.cursor()
-    cur.execute("delete from books where id=?",[request.args.get('id')])
-    conn.commit()
-    cur.close()
-    conn.close()
-    return redirect("livre?",303)
 
-@app.route('/modifier', methods=['POST'])
+# TODO: suppression d'un livre
+# cur.execute("delete from books where id=?", [request.args.get('id')])
+@app.route('/modifier', methods=['POST', 'GET'])
 def modifier():
-    conn=get_db_connection()
-    cur=conn.cursor()
-    cur.execute("") # TODO : compléter la requête
-    conn.commit()
-    cur.close()
-    conn.close()
-    return redirect("livre?",303)
+    if request.method == 'POST':
+        if request.args.get('id'):
+            conn = get_db_connection()
+            cur = conn.cursor()
+            cur.execute("update books set title=?, author=?, year=? where id=?",
+                        [request.form['title'], request.form['author'], request.form['year'], request.args.get('id')])
+            conn.commit()
+            cur.close()
+            conn.close()
+            return redirect("livre?id=" + request.args.get('id'), 303)
+        else:
+            conn = get_db_connection()
+            cur = conn.cursor()
+            cur.execute("insert into books (title, author, year) values (?, ?, ?); ",
+                        [request.form['title'], request.form['author'], request.form['year']])
+            idl = cur.lastrowid
+            conn.commit()
+            cur.close()
+            conn.close()
+            return redirect("livre?id=" + str(idl), 303)
+    else:
+        if request.args.get('id'):
+            conn = get_db_connection()
+            cur = conn.cursor()
+            data = cur.execute("select title, author, year from books where id=?", [request.args.get('id')]).fetchone()
+            cur.close()
+            conn.close()
+            return render_template('modifier.html', data=data, id=request.args.get('id'))
+        else:
+            return render_template('modifier.html', )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
